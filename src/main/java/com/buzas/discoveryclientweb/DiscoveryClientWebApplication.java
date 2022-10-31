3274e92f7cb3a273509990aa0c3b90ff180817b7
Vitovtas Buzas <vitavtasbuzas@gmail.com>
2022-10-31 11:08:01 +0300

    Initial commit

---
+++ src/main/java/com/buzas/discoveryclientweb/DiscoveryClientWebApplication.java
@@ -0,0 +1,13 @@
+package com.buzas.discoveryclientweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryClientWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryClientWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/buzas/discoveryclientweb/DiscoveryClientWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buzas.discoveryclientweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryClientWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
