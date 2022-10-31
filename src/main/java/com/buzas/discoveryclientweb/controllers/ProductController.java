1558b6106c36e13b799d66fc5baba6de562c917d
Vitovtas Buzas <vitavtasbuzas@gmail.com>
2022-10-31 12:01:52 +0300

    Init

--- src/main/java/com/buzas/discoveryclientweb/controllers/ProductController.java
+++ src/main/java/com/buzas/discoveryclientweb/controllers/ProductController.java
@@ -4,6 +4,7 @@ import com.buzas.discoveryclientweb.item.Product;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.PostConstruct;
@@ -13,14 +14,20 @@ import java.util.List;
 @RestController
 public class ProductController {
 
-    private List<Product> productList = new ArrayList<>();
+    private List<Product> productList;
+    private RestTemplate restTemplate;
+
+    public ProductController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     @PostConstruct
     private void Init() {
-        productList.add(new Product(0, "first", 15));
-        productList.add(new Product(1, "second", 72));
-        productList.add(new Product(2, "third", 133));
-        productList.add(new Product(3, "fourth", 40));
+        productList = new ArrayList<>();
+        productList.add(new Product(0L, "first", 15));
+        productList.add(new Product(1L, "second", 72));
+        productList.add(new Product(2L, "third", 133));
+        productList.add(new Product(3L, "fourth", 40));
     }
 
     @GetMapping("/")
--- src/main/java/com/buzas/discoveryclientweb/item/Product.java
+++ src/main/java/com/buzas/discoveryclientweb/item/Product.java
@@ -1,40 +1,17 @@
 package com.buzas.discoveryclientweb.item;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class Product {
-    private int id;
+    private Long id;
     private String title;
     private int price;
 
     public Product() {
-    }
-
-    public Product(int id, String title, int price) {
-        this.id = id;
-        this.title = title;
-        this.price = price;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
     }
 }
